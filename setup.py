e0025deeb1429515c75db23fbb0d225c7dc32449
Yin Cao <yincao@qti.qualcomm.com>
2021-09-09 20:45:04 +0800

    0.1.3 updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='panns_hear',
-    version='0.1.2',
+    version='0.1.3',
     description='PANNs embeddings for HEAR 2021 Challenge.',
     long_description=long_description,
     long_description_content_type="text/markdown",
